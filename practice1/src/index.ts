f7edcf8809bae6f61c8c3eb0f29bd23c3f162bfe
ChilenoSika <jaum97@yahoo.com.br>
2019-04-07 14:00:36 -0300

    Practice#1

--- practice1/src/index.ts
+++ practice1/src/index.ts
@@ -1,7 +1,30 @@
+const { log: l } = console;
+
 function greeter(person: string) {
-    return "Hello, " + person;
+    return typeof(person)==='string' 
+    ? "Hello, " + person
+    : new TypeError(`Expected String, received: ${person}`) 
 }
 
-let user = "Jane User";
+const user = "Jane User";
+const user2 = 3;
+const user3 = [0,1,2];
 
-document.body.innerHTML = greeter(user);
+l(greeter(user));
+l(greeter(user2));
+l(typeof(greeter(user3));
+l(greeter());
+
+
+
+if(greeter('Jane User') !== 'Hello, Jane User'){
+    throw new Error('Happy path failed')
+}
+
+if(!greeter(3) instanceof TypeError){
+    throw new Error('Type check failed')   
+}
+
+
+
+
